d37b8e39ba227e77f26ed5b660531f75f5a860d8
mvalmci <m.valenta@mci4me.at>
2025-04-29 16:55:10 +0200

    Abgabe: Leistungskurve 1.1

--- plot.py
+++ plot.py
@@ -6,20 +6,16 @@ def plot_data_array(x,y):
 
     plt.plot(x, y, label='Power Curve', color='blue')
     plt.xlabel('Time (min)') 
-    plt.ylabel('Power (W/kg)')
+    plt.ylabel('Power (W)')
     plt.title('Power Curve')
     plt.legend()
-    plt.grid(True)
-    plt.xlim(0, 30)  # Set x-axis limits to 0 to 60 minutes
+    plt.grid(True,which= 'both', linewidth=0.5)
+    plt.xlim(-1, 31)  # Set x-axis limits to 0 to 60 minutes
 
-    yticks = range(0, y, 2)
-    ytick_labels = [f"{val}W/kg" for val in yticks]
+    yticks = range(0, 433, 43)
+    ytick_labels = [f"{val}W" for val in yticks]
 
     plt.yticks(yticks, ytick_labels)
 
-
-
-
-
-
+    plt.savefig('power_curve.png')
     plt.show()
